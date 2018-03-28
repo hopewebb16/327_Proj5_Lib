45feb7dce059b1bfbcab9a9326c3ed9aa7f35656
Keith Perkins <CNUClasses@users.noreply.github.com>
2018-03-28 16:17:23 -0400

    Update Smalltalk_American.h

--- includes/Smalltalk_American.h
+++ includes/Smalltalk_American.h
@@ -8,8 +8,8 @@ class Smalltalk_American :
 public:
 	//create with appropriate phrases
 	//use base class call to set Nationality (See constants for Nationality strings)
-	Smalltalk_American(int i =1);
-	Smalltalk_American(std::string myNationality,int i =1);
+	Smalltalk_American(int iPerson=1);
+	Smalltalk_American(std::string myNationality,int iPerson=1);
 	
 	~Smalltalk_American(void);
 	virtual void populatePhrases();
